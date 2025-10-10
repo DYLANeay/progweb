820e976c798da0da8e4d666c63f52d75d710c9b2
DYLANeay <dylan.eray6@gmail.com>
2025-10-10 12:39:22 +0200

    challenge 6 working

---
+++ challenge6/script.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const languagePrompt = {
+  question: 'What is your favorite programming language',
+  options: [
+    '1. JavaScript',
+    '2. PHP',
+    '3. Java',
+    '4. Assembly',
+    '5. Rust',
+    '6. TypeScript',
+  ],
+};
+
+const coursePrompt = {
+  question: 'Ce challenge est-il difficile?',
+  options: ['Oui', 'Non'],
+};
+const createPoll = (prompt) => {
+  const votes = new Map();
+  prompt.options.forEach((name) => votes.set(name, 0));
+
+  const allKeys = Array.from(votes.keys());
+
+  //log the q and options on the console
+  console.log(prompt.question + ' : ' + allKeys.join(', '));
+
+  return {
+    vote(optionIndex) {
+      if (optionIndex - 1 >= 0 && optionIndex - 1 < prompt.options.length) {
+        const optionKey = allKeys[optionIndex - 1];
+        votes.set(optionKey, votes.get(optionKey) + 1);
+        console.log(
+          `Voted for: ${optionKey}, Actual votes: ${JSON.stringify([
+            ...votes.entries(),
+          ])}`
+        );
+      } else {
+        console.error('Invalid option index');
+      }
+    },
+    getResults() {
+      return votes;
+    },
+    getAllKeys() {
+      return votes.keys();
+    },
+  };
+};
+
+const languagePoll = createPoll(languagePrompt);
+languagePoll.vote(1);
+languagePoll.vote(3);
+
+const coursePoll = createPoll(coursePrompt);
+coursePoll.vote(1);
+coursePoll.vote(2);
+coursePoll.vote(1);
+coursePoll.vote(-1);
+
+console.log('Final results:', languagePoll.getResults());
+console.log('Final results:', coursePoll.getResults());
